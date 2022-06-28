bfe12745518ecfc6a5803c21b31e38fe301bc748
alihysn <78406246+alihysn@users.noreply.github.com>
2022-06-28 19:22:42 +0200

    update .h file

--- ThirtyTwoBit.h
+++ ThirtyTwoBit.h
@@ -1,5 +1,15 @@
 #pragma once
+#include <iostream>
+#include <fstream>
+#include "stdlib.h"
+#include <iomanip>
+#include <cstring>
+#include <string>
+#include <bits/stdc++.h>
+#include"ThirtyTwoBit.h"
+using namespace std;
 
 string SFormat(int unsigned InstWord);
-string BFormat(int unsigned InstWord);
+string BFormat(int unsigned InstWord,int unsigned PC);
 string UFormat(int unsigned InstWord);
+string JFormat(int unsigned InstWord,int unsigned PC)
